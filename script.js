bb027be5d02cd5dabba078fd82c206db18ed0827
Spencertainty <sjmfeagin@gmail.com>
2024-03-23 12:35:39 -0600

    updated console log, kept displaying "Rock:

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
+    console.log('Random index:', randomIndex);
+    console.log('Generated choice:', choices[randomIndex]);
     return choices[randomIndex];
 }
+
+const computerChoice = getComputerChoice();
+console.log('Computer choice:', computerChoice);
